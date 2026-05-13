e380b933c928974bc945b743151bb05d44fdcd55
guigesaifarm <guigesaifarm@gmail.com>
2026-05-13 13:02:55 -0300

    Create ClienteRepository interface for Cliente model

---
+++ ClienteRepository.java
@@ -0,0 +1,7 @@
+package com.ecommerce.repository;
+ 
+import com.ecommerce.model.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+ 
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+}
